c8c41dafa0e0f093286f287f0df721d121cc7648
Jon Haddad <jon@jonhaddad.com>
2015-04-29 15:26:08 -0700

    so close to working streams

--- src/main/python/pyspark_cassandra/__init__.py
+++ src/main/python/pyspark_cassandra/__init__.py
@@ -1,9 +1,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,6 +36,26 @@ pyspark.context.SparkContext = CassandraSparkContext
 
 # Monkey patch the default python RDD so that it can be stored to Cassandra as CQL rows
 pyspark.rdd.RDD.saveToCassandra = saveToCassandra
+
+# streaming support
+def saveDStreamToCassandra(
+        dstream, keyspace=None, table=None, columns=None,
+        batch_size=None, batch_buffer_size=None, batch_grouping_key=None,
+        consistency_level=None, parallelism_level=None, throughput_mibps=None,
+        ttl=None, timestamp=None, metrics_enabled=None, row_format=None
+):
+
+    def f(rdd):
+        return rdd.saveToCassandra(keyspace=keyspace, table=table, columns=columns,
+                            batch_size=batch_size, batch_buffer_size=batch_buffer_size, batch_grouping_key=batch_grouping_key,
+                            consistency_level=consistency_level, parallelism_level=parallelism_level, throughput_mibps=throughput_mibps,
+                            ttl=ttl, timestamp=timestamp, metrics_enabled=metrics_enabled, row_format=row_format)
+
+    return dstream.foreachRDD(f)
+
+
+import pyspark.streaming.dstream
+pyspark.streaming.dstream.DStream.saveToCassandra = saveDStreamToCassandra
 
 # Monkey patch the sc variable in the caller if any
 parent_frame = inspect.currentframe().f_back
